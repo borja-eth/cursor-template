3c127e48590b426c911fb1e8dd646fd904bc8a15
Andy Bardagí <andresbardagi2002@gmail.com>
2025-01-14 11:36:04 -0300

    feat: add unauthorized page

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import ErrorContainer from "@/components/common/Error/ErrorContainer";
+import ErrorContent from "@/components/common/Error/ErrorContent";
+import AstronautIcon from "@/components/icons/AstronautIcon";
+
+export default function Error() {
+    return (
+        <ErrorContainer>
+            <ErrorContent
+                icon={AstronautIcon}
+                message={"Oops! Something went wrong."}
+                title={"You are not authorized to see this page."}
+            />
+        </ErrorContainer>
+    );
+}
